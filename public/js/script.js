6ea19c4e9bb8a365d47ae7b76d87f6cea005d344
unknown <riou@viamundus.com>
2018-02-27 17:15:23 +0100

    adding toggleDrum and clear functions

--- public/js/script.js
+++ public/js/script.js
@@ -9,7 +9,7 @@ let rideCymbals = falseFill();
 // getArrayName Function to be able to integrate it inside the toggleDrum function
 const getArrayName = (name) => {
   if (name === 'kicks') {
-    return kick;
+    return kicks;
   } else if (name === 'snares') {
     return snares;
   } else if (name === 'hiHats') {
@@ -25,11 +25,20 @@ const getArrayName = (name) => {
 const toggleDrum = (arrayName, i) => {
   // we call here the getArrayName on toggleDrum attribute
   const drums = getArrayName(arrayName);
-
-
-  if (drum[i] === true) {
-    return drum[i] = false;
-  } else {
-    return drum[i] = true;
+  // we check the existency
+  if (!drums || i > 15 || i < 0) {
+    return;
   }
+  // we apply the function here
+  drums[i] = !drums[i];
 };
+
+// Clear Function
+const clear = (arrayName) => {
+  // we call here the getArrayName on toggleDrum attribute
+  const drums = getArrayName(arrayName);
+  // we apply the function here to array elements to false
+  if (drums) {
+    drums.fill(false);
+  }
+}
